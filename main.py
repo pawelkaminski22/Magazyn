5b74ca5bf0a9c3b35309ea8805b29bf6b13b33f6
Pawel Kaminski <pawelkaminski22@gmail.com>
2020-09-23 16:54:12 +0200

    Almost done

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
+import csv
+
 user_input = ''
-items = [{'Name': 'Milk', 'Quantity': 120, 'Unit': '    l', 'Unit Price (PLN)': 2.3},
+items = [{'Name': 'Milk', 'Quantity': 120, 'Unit': 'l', 'Unit Price (PLN)': 2.3},
          {'Name': 'Sugar', 'Quantity': 1000, 'Unit': 'Kg', 'Unit Price (PLN)': 3},
          {'Name': 'Flour', 'Quantity': 12000, 'Unit': 'Kg', 'Unit Price (PLN)': 1.2}
          ]
 sold_items = []
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 def get_items():
@@ -33,21 +30,57 @@ def add():
 
 def sell_item():
     name_sell = input('Item name:')
-    quantity_input = input('Quantity to sell:')
+    quantity_input = int(input('Quantity to sell:'))
     global sold_items
     for i in items:
         if i['Name'] == name_sell:
             new = i['Quantity'] - int(quantity_input)
-            i['Quantity'] = new
+            i['Quantity'] = int(new)
             unit = i['Unit']
             unit_price = i['Unit Price (PLN)']
-            sold_items = {'Name': name_sell, 'Quantity': quantity_input, 'Unit': unit, 'Unit Price (PLN)': unit_price}
+            sold_items = [{'Name': name_sell, 'Quantity': int(quantity_input), 'Unit': unit, 'Unit Price (PLN)': float(unit_price)}]
             print(f'Successfully sold {quantity_input} {unit} of {name_sell}')
             print(sold_items)
     get_items()
 
+
 def get_costs():
-    
+    costs = sum(item['Quantity'] * item['Unit Price (PLN)'] for item in items) * -1
+    income = sum(item['Quantity'] * item['Unit Price (PLN)'] for item in sold_items)
+    revenue = costs + income
+    print(f'Costs: {costs}')
+    print(f'Income: {income}')
+    print('____________')
+    print(f'Revenue: {revenue}')
+
+def export_items_to_csv():
+    with open('Magazyn.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Name', 'Quantity', 'Unit', 'Unit Price (PLN)']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerows(items)
+    print('Items saved')
+
+
+def export_sales_to_csv():
+    with open('Sales.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Name', 'Quantity', 'Unit', 'Unit Price (PLN)']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerows(sold_items)
+    print('Sales saved')
+
+
+def load_items_from_csv():
+    items.clear()
+    with open('Magazyn.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            items.append(row)
+    print(items)
+
 
 if __name__ == '__main__':
     while user_input != 'exit':
@@ -60,5 +93,15 @@ if __name__ == '__main__':
 
         elif user_input == 'sell':
             sell_item()
+
+        elif user_input == 'rev':
+            get_costs()
+
+        elif user_input == 'save':
+            export_items_to_csv()
+            export_sales_to_csv()
+
+        elif user_input == 'load':
+            load_items_from_csv()
 
     print('Exiting... Bye')
